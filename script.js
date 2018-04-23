c3f4ab92b041c6319748e70e68613d131a861233
Geprge-Prokopiou <george.prokopiou.andreas@gmail.com>
2018-04-23 11:15:41 +0300

    js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 //alert ('Hello world.');
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
